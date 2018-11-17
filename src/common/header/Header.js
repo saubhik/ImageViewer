1fd3638aeda9ac53136a84056040317187180e8a
joelvarghesejoy <joel.v@chaipoint.com>
2018-11-17 13:02:47 +0530

    Integerated with api and added filter functionality

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,11 +5,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import InputBase from '@material-ui/core/InputBase';
 import {withStyles} from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-import {fade} from '@material-ui/core/styles/colorManipulator';
 import Avatar from '@material-ui/core/Avatar';
-import AvatarIcon from '../../assets/ic_profile.png';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
+import IconButton from '@material-ui/core/IconButton';
 
 const theme = createMuiTheme({
   palette: {
@@ -28,17 +26,10 @@ const styles = theme => ({
   },
   search: {
     position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
+    borderRadius: '4px',
+    backgroundColor: '#c0c0c0',
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
-      width: 'auto'
-    }
+    width: '300px',
   },
   searchIcon: {
     width: theme.spacing.unit * 9,
@@ -47,7 +38,8 @@ const styles = theme => ({
     pointerEvents: 'none',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    color:'#000000'
   },
   inputInput: {
     paddingTop: theme.spacing.unit,
@@ -66,9 +58,9 @@ const styles = theme => ({
   avatar: {
     width: 50,
     height: 50,
-    margin: 10
   }
 })
+
 function Header(props) {
   const {classes, isSearchBarVisible, isProfileIconVisible} = props;
   return (<div>
@@ -82,14 +74,16 @@ function Header(props) {
               <div className={classes.searchIcon}>
                 <SearchIcon/>
               </div>
-              <InputBase placeholder="Search…" classes={{
+              <InputBase onChange={(e)=>{props.searchHandler(e.target.value)}} placeholder="Search…" classes={{
                   root: classes.inputRoot,
                   input: classes.inputInput
                 }}/>
             </div>
           }
           {isProfileIconVisible &&
-            <Avatar alt="Remy Sharp" src={AvatarIcon} className={classes.avatar}/>
+            <IconButton>
+              <Avatar alt="Profile Pic" src={props.userProfileUrl} className={classes.avatar}/>
+            </IconButton>
           }
         </Toolbar>
       </AppBar>
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -8,7 +8,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
-import AvatarIcon from '../../assets/ic_profile.png';
 import {withStyles} from '@material-ui/core/styles';
 import FavoriteIconBorder from '@material-ui/icons/FavoriteBorder';
 import FavoriteIconFill from '@material-ui/icons/Favorite';
@@ -19,6 +18,7 @@ import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import {constants} from '../../common/utils'
 
 const styles =  theme => ({
   card: {
@@ -46,163 +46,33 @@ class Home extends Component{
   constructor() {
     super();
     sessionStorage.setItem('username','admin');
+    sessionStorage.setItem('access-token', '8661035776.d0fcd39.87fd934e04f84253aaf234d8bd4e4c65');
     this.state = {
-      "data": [
-        {
-          "comments": {
-            "count": 0
-          },
-          "caption": {
-            "created_time": "1296710352",
-            "text": "Inside le truc #foodtruck",
-            "from": {
-              "username": "kevin",
-              "full_name": "Kevin Systrom",
-              "type": "user",
-              "id": "3"
-            },
-            "id": "26621408"
-          },
-          "likes": {
-            "count": 15
-          },
-          "link": "http://instagr.am/p/BWrVZ/",
-          "user": {
-            "username": "kevin",
-            "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_3_75sq_1295574122.jpg",
-            "id": "3"
-          },
-          "created_time": "1296710327",
-          "images": {
-            "low_resolution": {
-              "url": "http://distillery.s3.amazonaws.com/media/2011/02/02/6ea7baea55774c5e81e7e3e1f6e791a7_6.jpg",
-              "width": 306,
-              "height": 306
-            },
-            "thumbnail": {
-              "url": "http://distillery.s3.amazonaws.com/media/2011/02/02/6ea7baea55774c5e81e7e3e1f6e791a7_5.jpg",
-              "width": 150,
-              "height": 150
-            },
-            "standard_resolution": {
-              "url": "http://distillery.s3.amazonaws.com/media/2011/02/02/6ea7baea55774c5e81e7e3e1f6e791a7_7.jpg",
-              "width": 612,
-              "height": 612
-            }
-          },
-          "type": "image",
-          "users_in_photo": [],
-          "filter": "Earlybird",
-          "tags": ["foodtruck"],
-          "id": "22721881",
-          "location": {
-            "latitude": 37.778720183610183,
-            "longitude": -122.3962783813477,
-            "id": "520640",
-            "street_address": "",
-            "name": "Le Truc"
-          }
-        },
-        {
-          "comments": {
-            "count": 0
-          },
-          "caption": {
-            "created_time": "1296710352",
-            "text": "Inside le truc #foodtruck",
-            "from": {
-              "username": "kevin",
-              "full_name": "Kevin Systrom",
-              "type": "user",
-              "id": "3"
-            },
-            "id": "26621409"
-          },
-          "likes": {
-            "count": 15
-          },
-          "link": "http://instagr.am/p/BWrVZ/",
-          "user": {
-            "username": "kevin",
-            "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_3_75sq_1295574122.jpg",
-            "id": "3"
-          },
-          "created_time": "1296710327",
-          "images": {
-            "low_resolution": {
-              "url": "http://distillery.s3.amazonaws.com/media/2011/02/02/6ea7baea55774c5e81e7e3e1f6e791a7_6.jpg",
-              "width": 306,
-              "height": 306
-            },
-            "thumbnail": {
-              "url": "http://distillery.s3.amazonaws.com/media/2011/02/02/6ea7baea55774c5e81e7e3e1f6e791a7_5.jpg",
-              "width": 150,
-              "height": 150
-            },
-            "standard_resolution": {
-              "url": "http://distillery.s3.amazonaws.com/media/2011/02/02/6ea7baea55774c5e81e7e3e1f6e791a7_7.jpg",
-              "width": 612,
-              "height": 612
-            }
-          },
-          "type": "image",
-          "users_in_photo": [],
-          "filter": "Earlybird",
-          "tags": ["foodtruck"],
-          "id": "22721882",
-          "location": {
-            "latitude": 37.778720183610183,
-            "longitude": -122.3962783813477,
-            "id": "520640",
-            "street_address": "",
-            "name": "Le Truc"
-          }
-        }
-      ],
+      "data": [],
+      filteredData:[],
+      userData:{},
       likeSet:new Set(),
-      comments:{
-        22721881:['lafhlsaf','asfkgisag']
-      },
+      comments:{},
       currrentComment:""
     }
 }
 
   componentDidMount(){
-    // let url = ``;
-    // return fetch(url,{
-    //   method:'GET',
-    //   headers: {
-    //     'authorization' : this.props.Auth.auth
-    //   }
-    //
-    // }).then((response) =>{
-    //   that.setState({isLoading:false});
-    //   if (response.status == 401) {
-    //     that.logout();
-    //   } else {
-    //     return response.json();
-    //   }
-    // }).then((jsonResponse) =>{
-    //   if (jsonResponse && jsonResponse.status == "3") {
-    //     that.paymentComplete(jsonResponse);
-    //   }else {
-    //     that.handleError();
-    //   }
-    // }).catch((error) => {
-    //   that.handleError();
-    // });
+    this.getUserInfo();
+    this.getMediaData();
   }
 
   render(){
     const{classes} = this.props;
-
     return(
       <div>
         <Header
+          userProfileUrl={this.state.userData.profile_picture}
           isSearchBarVisible={true}
-          isProfileIconVisible={true}/>
+          isProfileIconVisible={true}
+          searchHandler={this.onSearchEntered}/>
         <GridList cellHeight={'auto'} spacing={5}>
-          {this.state.data.map(item => (
+          {this.state.filteredData.map(item => (
             <GridListTile key={item.id}>
               <HomeItem
                 classes={classes}
@@ -218,6 +88,19 @@ class Home extends Component{
         </GridList>
       </div>
     );
+  }
+
+  onSearchEntered = (value) =>{
+    console.log('search value', value);
+    let filteredData = this.state.data;
+    filteredData = filteredData.filter((data) =>{
+      let string = data.caption.text.toLowerCase();
+      let subString = value.toLowerCase();
+      return string.includes(subString);
+    })
+    this.setState({
+      filteredData
+    })
   }
 
   likeClickHandler = (id) =>{
@@ -250,6 +133,9 @@ class Home extends Component{
 
   addCommentClickHandler = (id)=>{
     console.log('coment id',id,this.state.currentComment, this.state.comments);
+    if (this.state.currentComment === "" || this.state.currentComment === undefined) {
+      return;
+    }
 
     let commentList = this.state.comments.hasOwnProperty(id)?
       this.state.comments[id].concat(this.state.currentComment): [].concat(this.state.currentComment);
@@ -268,6 +154,39 @@ class Home extends Component{
   commentChangeHandler = (e) => {
     this.setState({
       currentComment:e.target.value
+    });
+  }
+
+  getUserInfo = () => {
+    let that = this;
+    let url = `${constants.userInfoUrl}/?access_token=${sessionStorage.getItem('access-token')}`;
+    return fetch(url,{
+      method:'GET',
+    }).then((response) =>{
+        return response.json();
+    }).then((jsonResponse) =>{
+      that.setState({
+        userData:jsonResponse.data
+      });
+    }).catch((error) => {
+      console.log('error user data',error);
+    });
+  }
+
+  getMediaData = () => {
+    let that = this;
+    let url = `${constants.userMediaUrl}/?access_token=${sessionStorage.getItem('access-token')}`;
+    return fetch(url,{
+      method:'GET',
+    }).then((response) =>{
+        return response.json();
+    }).then((jsonResponse) =>{
+      that.setState({
+        data:jsonResponse.data,
+        filteredData:jsonResponse.data
+      });
+    }).catch((error) => {
+      console.log('error user data',error);
     });
   }
 }
@@ -293,14 +212,14 @@ function HomeItem(props) {
       <Card className={classes.card}>
         <CardHeader
           avatar={
-            <Avatar alt="Remy Sharp" src={AvatarIcon} className={classes.avatar}/>
+            <Avatar alt="User Profile Pic" src={item.user.profile_picture} className={classes.avatar}/>
           }
           title={item.user.username}
           subheader={time}
         />
         <CardMedia
           className={classes.media}
-          image={AvatarIcon}
+          image={item.images.standard_resolution.url}
           title={item.caption.text}
         />
         <CardContent>
